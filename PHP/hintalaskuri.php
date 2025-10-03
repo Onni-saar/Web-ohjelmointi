3becf7d17447cc90fd87c205edd656e79be8a638
Onni-saar <Onni.Saarberg@Vuoksi.fi>
2025-10-03 08:28:05 +0300

    newchanges

---
+++ PHP/hintalaskuri.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <title>Hintalaskuri</title>
+</head>
+<body>
+    <h1></h1>
+    <?php
+    // 1. Muuttujien määrittely
+    // 2. Laskutoimitukset
+    // 3. Tietojen tulostus selaimelle
+    // number_format() muotoilee luvun. Esim: number_format(luku, desimaalien_määrä, desimaalierotin, tuhaterotin)
+    ?>
+</body>
+</html>
